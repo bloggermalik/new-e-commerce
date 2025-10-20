21c7a95843f9b66092e46733f93d2c31182484a3
MAlik <timepasstime123@gmail.com>
2025-10-20 19:48:25 +0530

    lint error corrected

--- zBackupApp/app/action.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from 'next/headers';
-
-const FLASH_COOKIE_NAME = 'flash_message';
-
-
-export async function deleteFlashMessage(): Promise<void> {
-  const cookieStore = await cookies(); // 👈 wait for the Promise
-  cookieStore.delete(FLASH_COOKIE_NAME);
-}
