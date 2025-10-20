8158dbe2ccecfe4a1e05265c6bdb5cddf164d0dd
MAlik <timepasstime123@gmail.com>
2025-10-20 19:21:23 +0530

    lint error corrected

--- components/add-user-button.tsx
+++ components/add-user-button.tsx
@@ -1,7 +1,29 @@
+// "use client"
+// import React from 'react'
+// import { Button } from './ui/button'
+// import { createUser } from '@/server/user'
+
+
+// function AddUserButton() {
+
+//     const [loading, setLoading] = React.useState(false);
+//   return (
+//     <div>
+//       <Button onClick={async ()=>{
+//           setLoading(true);
+//           await createUser();
+//           setLoading(false);
+//       }} className='max-w-sm' disabled={loading}>
+//         {loading ? "Loading..." : "Add New User"}
+//       </Button>
+//     </div>
+//   )
+// }
+
+// export default AddUserButton
 "use client"
 import React from 'react'
 import { Button } from './ui/button'
-import { createUser } from '@/server/user'
 
 
 function AddUserButton() {
@@ -11,7 +33,6 @@ function AddUserButton() {
     <div>
       <Button onClick={async ()=>{
           setLoading(true);
-          await createUser();
           setLoading(false);
       }} className='max-w-sm' disabled={loading}>
         {loading ? "Loading..." : "Add New User"}
