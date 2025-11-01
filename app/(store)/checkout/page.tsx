7aefb4f797679fb7b3294d1e9079e295226e33b2
MAlik <timepasstime123@gmail.com>
2025-11-01 14:21:37 +0530

    cart working

--- app/(store)/checkout/page.tsx
+++ app/(store)/checkout/page.tsx
@@ -2,8 +2,10 @@ import getProfileByUserId, { getCart, getSession } from '@/server/user';
 import getQueryClient from "@/app/(dashboard)/admin/provider/get-query-client"
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
 import CartPage from '@/components/ui/cart-page';
-import { Alert, Button } from '@mui/material';
-import Person2Icon from '@mui/icons-material/Person2';
+import { Alert, } from '@mui/material';
+import EastIcon from '@mui/icons-material/East';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export const dynamic = "force-dynamic";
 
@@ -15,32 +17,44 @@ export default async function page() {
   const queryClient = getQueryClient()
 
   // Prefetch categories data on the server with proper caching configuration
-const [profileData, cartData] = await Promise.all([
-  queryClient.fetchQuery({
-    queryKey: ["profile", userId],
-    queryFn: () => getProfileByUserId(userId!),
-    staleTime: 0,
-  }),
-  queryClient.fetchQuery({
-    queryKey: ["cart", userId],
-    queryFn: () => getCart(),
-    staleTime: 0,
-  }),
-]);
+  const [profileData, cartData] = await Promise.all([
+    queryClient.fetchQuery({
+      queryKey: ["profile", userId],
+      queryFn: () => getProfileByUserId(userId!),
+      staleTime: 0,
+    }),
+    queryClient.fetchQuery({
+      queryKey: ["cart", userId],
+      queryFn: () => getCart(),
+      staleTime: 0,
+    }),
+  ]);
 
-console.log("Profile data:", profileData);
-console.log("Cart data:", cartData);
+  console.log("Profile data:", profileData);
+  console.log("Cart data:", cartData);
 
-if(profileData?.mobile === "" || profileData?.address === "") {
-  return (
-    <div className="space-y-4 flex flex-col h-[50vh] items-center justify-center text-center text-red-500">
-      <Alert severity="warning">Please update your profile before checkout.</Alert>
-       <Button  href='/profile' variant="contained" endIcon={<Person2Icon />}>
-        Update Profile
-      </Button>
-    </div>
-  );
-}
+  if (profileData?.mobile === "" || profileData?.address === "") {
+    return (
+      <div className="space-y-4 mt-40 p-8 max-w-[600px] mx-auto flex flex-col h-[50vh] text-center text-red-500">
+        <Alert severity="warning">Please update your profile before checkout.</Alert>
+
+        <div className="flex justify-end w-full">
+          <Link href="/profile">
+            <Button
+              variant="outline"
+              color="outline"
+              className="w-[160px] mb-10 text-sm font-semibold border-primary text-primary bg-white hover:bg-primary hover:text-white transition-colors"
+            >
+              Update Profile
+              <EastIcon className="" />
+
+            </Button>
+          </Link>
+        </div>
+      </div>
+
+    );
+  }
 
 
   return (
@@ -48,7 +62,7 @@ if(profileData?.mobile === "" || profileData?.address === "") {
 
       <div >
         <CartPage />
-     
+
       </div>
     </HydrationBoundary>
   )
