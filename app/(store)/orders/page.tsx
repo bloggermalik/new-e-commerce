bffe5843daffc5bda9c4b753f6ab857a7985b670
MAlik <timepasstime123@gmail.com>
2025-11-01 12:13:26 +0530

    cart working

--- app/(store)/orders/page.tsx
+++ app/(store)/orders/page.tsx
@@ -3,8 +3,193 @@ export const dynamic = "force-dynamic";
 
 export default function Orders() {
     return (
+        <>
         <div>
             This is the orders page.
         </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div>
+        <div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+            This is the orders page.
+        </div><div>
+           LAST Order.
+        </div>
+
+        </>
     )
 }
