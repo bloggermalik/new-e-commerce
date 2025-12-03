22aaae1ce826d337b831e11dc07679037e032c0d
bloggermalik <bloggermalik@gmail.com>
2025-12-03 20:39:19 +0530

    working

---
+++ app/src/main/java/app/lenzoa/com/DelegationService.java
@@ -0,0 +1,14 @@
+package app.lenzoa.com;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
